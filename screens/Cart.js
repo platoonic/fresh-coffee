2aa6a3e207853c47e4adbbc590caf294b140ffa6
Khalid Khalil <coderizer@aol.com>
2020-07-24 18:57:11 +0200

    Integrate Cart component with Redux store

--- screens/Cart.js
+++ screens/Cart.js
@@ -6,8 +6,9 @@ import { useFonts } from "expo-font";
 import Button from "../components/UI/Button";
 // Redux
 import { connect } from "react-redux";
+import { getCartItems } from "../redux/selectors/cartItems";
 
-function Cart({ cartItems }) {
+function Cart({ cartItems, cartTotal }) {
   // Custom Font (FuturaBT)
   const [fontsLoaded] = useFonts({
     "FuturaBT-Medium": require("../assets/fonts/FuturaBT-Medium.ttf"),
@@ -23,23 +24,26 @@ function Cart({ cartItems }) {
   }
 
   // Cart component state
-  const [total, setTotal] = useState("25.00");
   const deliveryCharges = "10.00";
+  const total = (Number(cartTotal) + Number(deliveryCharges)).toFixed(2);
 
-  // Calculate total price of cart items
-  useEffect(() => {
-    const totalPrice = cartItems.reduce((sum, item) => {
-      const price = Number(item.price);
-      return sum + price;
-    }, 0);
-    setTotal((totalPrice + Number(deliveryCharges)).toFixed(2));
-  }, [cartItems]);
+  // Handle empty Cart
+  if (cartItems.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text style={[FuturaBT, { fontSize: 19, alignSelf: "center" }]}>
+          Your Cart is Empty!
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.cartItems}>
         {cartItems.map((item) => {
           return (
-            <View style={styles.item} key={item.name}>
+            <View style={styles.item} key={item.id}>
               <Text style={[FuturaBT, styles.title]}>{item.name}</Text>
               <View style={styles.quantity}>
                 <Text
@@ -125,8 +129,12 @@ const styles = StyleSheet.create({
   },
 });
 
-const mapStateToProps = (state) => ({
-  cartItems: state.cartReducer,
-});
+const mapStateToProps = (state) => {
+  const { cartItems, cartTotal } = getCartItems(state);
+  return {
+    cartItems,
+    cartTotal,
+  };
+};
 
 export default connect(mapStateToProps, null)(Cart);
