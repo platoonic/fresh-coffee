32aa8e435ba46bd5e2af0b1c31024ef549eac0b1
Khalid Khalil <coderizer@aol.com>
2020-07-25 03:34:58 +0200

    Add increase/decrease quantity Redux Action

--- redux/actions/cart.js
+++ redux/actions/cart.js
@@ -13,3 +13,13 @@ export const removeFromCart = (itemID) => ({
   type: "REMOVE_FROM_CART",
   itemID,
 });
+
+export const increaseQuantity = (itemID) => ({
+  type: "INCRASE_QUANTITY",
+  itemID,
+});
+
+export const decreaseQuantity = (itemID) => ({
+  type: "DECREASE_QUANTITY",
+  itemID,
+});
